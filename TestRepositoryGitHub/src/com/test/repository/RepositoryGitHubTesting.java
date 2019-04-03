5981215ede65907291eb4cba21acd6e2d76fb6fa
Judith Aio <Judith Aio@DESKTOP-KSD455U.fios-router.home>
2019-04-02 21:06:18 -0400

    New code line

---
+++ TestRepositoryGitHub/src/com/test/repository/RepositoryGitHubTesting.java
@@ -0,0 +1,41 @@
+package com.test.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositoryGitHubTesting {
+
+	
+	public static void main(String[] args) {
+
+
+        List<String> verticesposition2 = new ArrayList<String>();
+
+        verticesposition2.add("safdsfVertex");
+
+        verticesposition2.add("safdsfNamesfsd");
+     verticesposition2.add("notCONTAINS");
+        verticesposition2.add(" shoes");
+
+        for (String str5 : verticesposition2){
+
+            if((str5.contains("Vertex")||str5.contains("Name")||str5.contains("Transmittance")||str5.contains("shoes"))){
+
+            	
+            	
+            	
+            	
+            	int num = 2;
+            	
+            	int num2 = 5;
+            	
+            	
+	
+            }
+        }
+}
+	
+	
+	
+	
+}
